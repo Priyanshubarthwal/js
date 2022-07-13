23805980c006b02cd24b89ef8f7f24a597a74987
Priyanshubarthwal <103275278+Priyanshubarthwal@users.noreply.github.com>
2022-07-13 22:21:51 +0530

    Create Kruskak.js

---
+++ Kruskak.js
@@ -0,0 +1,48 @@
+var V = 5;
+        var parent = Array(V).fill(0);
+        var INF = 1000000000;
+
+        function find(i){
+	        while (parent[i] != i)
+		    i = parent[i];
+	        return i;
+        }
+
+        function union1(i, j){
+	        var a = find(i);
+	        var b = find(j);
+	        parent[a] = b;
+        }
+
+        function kruskalMST(cost){
+	        var mincost = 0;
+	        for (var i = 0; i < V; i++)
+		        parent[i] = i;
+
+	        var edge_count = 0;
+	        while (edge_count < V - 1){
+		        var min = INF, a = -1, b = -1;
+		        for (var i = 0; i < V; i++){
+			        for (var j = 0; j < V; j++){
+				        if (find(i) != find(j) && cost[i][j] < min){
+					        min = cost[i][j];
+					        a = i;
+					        b = j;
+				        }
+			        }
+		        }
+
+		        union1(a, b);
+                console.log(`Edge ${edge_count++}:(${a},${b}) cost:${min}`);
+		        mincost += min;
+	        }
+	        console.log(`Minimum cost= ${mincost}`);
+        }
+
+        var cost = [
+	        [INF    , 2, INF, 6, INF],
+	        [2, INF, 3, 8, 5],
+	        [INF, 3, INF, INF, 7],
+	        [6, 8, INF, INF, 9],
+	        [INF, 5, 7, 9, INF]];
+        kruskalMST(cost);
